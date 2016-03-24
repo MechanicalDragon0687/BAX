d60714fcda0324e1f139d82c64c7457d41ad4970
Wolfvak <soherrera1@hotmail.com>
2016-03-24 10:15:32 -0300

    Updated to v0.2
    Added a 'variable framerate' configuration, changed folder to "anim" instead of root directory and changed the white screen to black

--- source/draw.c
+++ source/draw.c
@@ -19,8 +19,8 @@ u32 fb_sz(u8* fb) {
 }
 
 void clearScreen() {
-	memset(framebuffers->top_left, 0xFF, TOP_FB_SZ);
-	memset(framebuffers->bottom, 0xFF, BOTTOM_FB_SZ);
+	memset(framebuffers->top_left, 0x00, TOP_FB_SZ);
+	memset(framebuffers->bottom, 0x00, BOTTOM_FB_SZ);
 }
 
 u32 max(u32 n_1, u32 n_2) {
@@ -28,35 +28,55 @@ u32 max(u32 n_1, u32 n_2) {
 	else return n_2;
 }
 
+void delay(u32 n) {
+	u32 i = n; while (i--) __asm("andeq r0, r0, r0"); // ASM NOP(e), supposed to delay animation
+}
+
 void drawBootScreen() {
 	clearScreen(); // clear the screen
-	char top_anim[] = "/anim", bottom_anim[] = "/bottom_anim"; // define file names
-	u32 topAnimSize, topFrames, bottomAnimSize, bottomFrames; // define variables
+	char config[] = "/anim/config", top_anim[] = "/anim/anim", bottom_anim[] = "/anim/bottom_anim"; // define file names
+	u8 rate[1];
+	u32 topAnimSize, topFrames = 0, bottomAnimSize, bottomFrames = 0;//, frameRate; // define variables
 
 	topAnimSize = fileSize(top_anim); // get top screen animation size
 	bottomAnimSize = fileSize(bottom_anim); // get bottom screen animation size
+	u32 configSize = fileSize(config);
 
 	if ((topAnimSize + bottomAnimSize) >= MAX_SIZE || (topAnimSize+bottomAnimSize) == 0) // return if higher than 64MB or non-existant
 		return;
 
-	topFrames = (topAnimSize / TOP_FB_SZ); // get top screen frames
-	bottomFrames = (bottomAnimSize / BOTTOM_FB_SZ); // get bottom screen frames
+	topFrames = ((topAnimSize - 1) / TOP_FB_SZ); // get top screen frames
+	bottomFrames = ((bottomAnimSize - 1) / BOTTOM_FB_SZ); // get bottom screen frames
 
 	fileRead((u8*)LOAD_ADDR, top_anim, topAnimSize); // read the top animation to LOAD_ADDR
 	fileRead((u8*)LOAD_ADDR + topAnimSize, bottom_anim, bottomAnimSize); // read the bottom animation to (LOAD_ADDR + topAnimSize)
 
+	fileRead(rate, config, configSize); // read framerate
+
 	u32 frames = max(topFrames, bottomFrames); // get the maximum amount of frames between the two animations
+
+	u32 delay_ = (6990480 / rate[0]); // need to take more accurate measurements, but this will do, it's quite a magic number
+
+	u32 delay__ = (delay_ / 2);
 
 	for (u32 curframe = 0; curframe < frames; curframe++) { // loop until the maximum amount of frames, increasing frame count by 1
 
-		if (topAnimSize != 0 && curframe < topFrames) // if top animation exists and hasn't ended yet
+		if (topAnimSize != 0 && curframe < topFrames) {// if top animation exists and hasn't ended yet
 			memcpy((void*)framebuffers->top_left, (void*)LOAD_ADDR + (curframe*TOP_FB_SZ), TOP_FB_SZ); // refresh top animation
+			if (curframe <= bottomFrames) // check whether the bottom animation is playing
+				delay(delay__); // half the delay
+			else
+				delay(delay_); // whole delay
+		}
 
-		if (bottomAnimSize != 0 && curframe < bottomFrames) // if bottom animation exists and hasn't ended yet
+		if (bottomAnimSize != 0 && curframe < bottomFrames) {// if bottom animation exists and hasn't ended yet
 			memcpy((void*)framebuffers->bottom, (void*)LOAD_ADDR + topAnimSize + (curframe*BOTTOM_FB_SZ), BOTTOM_FB_SZ); // refresh bottom animation
-
-		u32 i = 0x1AAAAA; while (i--) __asm("andeq r0, r0, r0"); // ASM NOP(e), supposed to delay animation
-		// THIS HAS BEEN CALIBRATED FOR 10 FPS
+			if (curframe <= topFrames) // check whether the top animation is playing
+				delay(delay__); // half the delay
+			else
+				delay(delay_); // whole delay
+		}
+		// THIS HAS BEEN *not* CAREFULLY CALIBRATED
 	}
 	return;
 }
